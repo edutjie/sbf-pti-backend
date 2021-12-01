4fbe98e0afb2f32ba1adfe84356f0745ea8bc3db
VOIZCRACK <58902925+AdrianRamadhan27@users.noreply.github.com>
2021-12-01 21:54:17 +0700

    Added endpoint for delete

--- SBFlix/urls.py
+++ SBFlix/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.MovieListView.as_view()),
     path('add_movie/', views.MovieCreateView.as_view()),
+    path('<int:pk>/delete/', views.MovieDestroy.as_view()),
 ]
