df3d06d30219940a541530ec127faf68a41352b7
edutjie <69960893+edutjie@users.noreply.github.com>
2021-12-07 19:23:45 +0700

    Final Backend

--- SBFlix/urls.py
+++ SBFlix/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.MovieListView.as_view()),
-    path('add_movie/', views.MovieCreateView.as_view()),
-    path('<int:pk>/delete/', views.MovieDestroy.as_view()),
+    path('filter/', views.MovieFilter.as_view()),
+    path('search/', views.MovieSearch.as_view()),
+    path('<int:pk>/delete/', views.MovieDestroyView.as_view()),
     path('add_like/<int:id>', views.Likes.as_view()),
     path('add_dislike/<int:id>', views.Dislikes.as_view()),
 ]
--- SBFlix/views.py
+++ SBFlix/views.py
@@ -1,7 +1,7 @@
 from django.db.models import query
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Movie
@@ -13,16 +13,41 @@ from django.core.exceptions import ValidationError
 
 
 # Create your views here.
-class MovieListView(ListAPIView):
+class MovieListView(ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
-    filterset_fields = ['title','genre']
-    ordering_fields = ['title','genre']
-    search_fields = ['title']
     
-class MovieCreateView(CreateAPIView):
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializers = MovieSerializer(queryset, many=True)
+        return Response({
+            "status" : 200,
+            "message" : "Success",
+            "data":serializers.data
+        })
+    
+    def perform_create(self, serializer):
+        queryset = Movie.objects.filter(title=self.request.data["title"])
+        if queryset.exists():
+            # handle if the movie is already exists
+            raise ValidationError('the movie is already exists')
+        serializer.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+class MovieFilter(ListAPIView):
+    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['title','genre','likes']
+    ordering_fields = ['title','genre','likes']
+    # url/SBFlix/filter/?ordering=genre,-likes,title to get query grouped by genre, ordered by likes desc and title asc
+    
+class MovieSearch(ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
+    # url/SBFlix/search/?search=movietitle change the movietitle according to the title you want to search
     
 class MovieDestroyView(DestroyAPIView):
     queryset = Movie.objects.all()
